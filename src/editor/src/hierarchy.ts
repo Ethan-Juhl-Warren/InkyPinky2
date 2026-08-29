b6919368fccdadea651bd4b1bb3b13519a758174
Angelo Bastos <ugandan.meow@gmail.com>
2026-08-29 22:07:17 +0200

    del stale todos

--- src/editor/src/hierarchy.ts
+++ src/editor/src/hierarchy.ts
@@ -6,11 +6,6 @@ export interface HierarchyOptions {
   container: HTMLElement;
 }
 
-// TODO: get the entity list from sceneStub.getEntities().
-// TODO: draw one row per entity, showing its name.
-// TODO: highlight whichever entity is currently selected.
-// TODO: clicking a row calls sceneStub.select(id).
-// TODO: re-draw the list whenever sceneStub.subscribe() fires.
 export function mountHierarchy(options: HierarchyOptions): void {
 
   function render(): void {
